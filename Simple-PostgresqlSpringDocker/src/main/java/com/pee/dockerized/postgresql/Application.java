9c36baa8983e4928f811cc1e3db006614ca8d8a7
DeTiveNC <116792124+DeTiveNC@users.noreply.github.com>
2024-03-11 22:54:27 +0100

    ⚙️ Spring Template Dockerized v2 - Small changes

--- Simple-PostgresqlSpringDocker/src/main/java/com/pee/dockerized/postgresql/Application.java
+++ Simple-PostgresqlSpringDocker/src/main/java/com/pee/dockerized/postgresql/Application.java
@@ -4,30 +4,17 @@ import com.pee.dockerized.postgresql.entity.Role;
 import com.pee.dockerized.postgresql.entity.User;
 import com.pee.dockerized.postgresql.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 	@Autowired
 	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		User adminAccount = userRepository.findByRole(Role.ADMIN);
-		if (adminAccount == null) {
-			User admin = new User();
-			admin.setEmail("admin@admin.com");
-			admin.setRole(Role.ADMIN);
-			admin.setPassword(new BCryptPasswordEncoder().encode("admin"));
-			userRepository.save(admin);
-		}
 	}
 }
