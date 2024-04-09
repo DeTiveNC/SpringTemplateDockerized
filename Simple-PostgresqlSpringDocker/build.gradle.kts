386d0518cd86ba114bfb18f335dc16174efd575e
DeTiveNC <nicolascao12@gmail.com>
2024-04-10 00:28:58 +0200

    Part 2: Creating the gradle.yml

--- Simple-PostgresqlSpringDocker/build.gradle.kts
+++ Simple-PostgresqlSpringDocker/build.gradle.kts
@@ -49,9 +49,10 @@ dependencies {
 }
 
 group = "com.pee"
-version = "2.0.0"
+version = "2.5.0"
 description = "dockerized.postgresql"
 java.sourceCompatibility = JavaVersion.VERSION_21
+
 
 publishing {
     publications.create<MavenPublication>("maven") {
